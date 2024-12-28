efab0a2b668113fe97fac88c7b6d0e5d55b4b65c
MovieBots <154045205+MovieBots@users.noreply.github.com>
2024-12-28 20:35:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ class Rkn_Bots(object):
     PORT = int(os.environ.get("PORT", "8080"))
 
     # force subs channel ( required.. 😥)
-    FORCE_SUB = os.environ.get("FORCE_SUB", "Rkn_Botz") 
+    FORCE_SUB = os.environ.get("FORCE_SUB", "MOVIES_WORLDZSS") 
     
     # database config ( required.. 😥)
     DB_NAME = os.environ.get("DB_NAME", "AutoCaption_V05_Bot")     
